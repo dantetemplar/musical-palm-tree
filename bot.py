64faeaa1b667600203007c1351fb4ce331188286
Ruslan Bel'kov <ruslan.belckov@yandex.ru>
2023-06-11 10:46:30 +0300

    feat(echo-bot): Second version
    Бот запоминает всех пользователей, и пересылает сообщение от каждого
    пользователя каждому друго пользователю (в том числе себе самому)

--- bot.py
+++ bot.py
@@ -15,14 +15,28 @@ logging.basicConfig(level=logging.INFO)
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot, storage=MemoryStorage())
 
+all_users = set()
+
 
 @dp.message_handler(commands=['start'], state="*")
 async def _(message: Message, state: FSMContext):
-    await state.update_data({"user_id": message.from_user.id})
+    user_id = message.from_user.id
+
+    # Запоминаем всех пользователей, что нажали /start
+    if user_id not in all_users:
+        all_users.add(user_id)
+        await send_to_all_users(f"Всего пользователей: {len(all_users)}")
+
+    await state.update_data({"user_id": user_id, "username": message.from_user.username})
     await message.answer("Привет! Напиши мне что-нибудь!")
     # то же самое что и
     # await bot.send_message(message.from_user.id, "Привет! Напиши мне что-нибудь!")
     await state.set_state("wait_for_message")
+
+
+async def send_to_all_users(text, parse_mode=None):
+    for user_id in tuple(all_users):
+        await bot.send_message(user_id, text, parse_mode=parse_mode)
 
 
 @dp.message_handler(state="wait_for_message")
@@ -33,10 +47,12 @@ async def _(message: Message, state: FSMContext):
     # text = f"@{message.from_user.username} написал(а):\n{message.text}"
 
     text = \
-        f"<a href='tg://user?id={message.from_user.id}'>"\
+        f"<a href='tg://user?id={message.from_user.id}'>" \
         f"пользователь</a> написал(а):\n{message.text}"
 
-    await message.answer(text, parse_mode="HTML")
+    await send_to_all_users(text, parse_mode="HTML")
+
+    # await message.answer(text, parse_mode="HTML")
 
 
 if __name__ == '__main__':
