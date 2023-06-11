60dba43e86a56c24b8b755dc825cf17ee45beabc
Ruslan Bel'kov <ruslan.belckov@yandex.ru>
2023-06-11 11:01:32 +0300

    feat(echo-bot): Third version
    Бот запоминает всех пользователей, и пересылает сообщение от
    каждого
    пользователя каждому друго пользователю (но не присылает самому
    себе)
    
    Также не отправляем токен на гитхаб

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 import logging
+from typing import Iterable
 
 from aiogram import Bot, Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
@@ -20,37 +21,42 @@ all_users = set()
 
 @dp.message_handler(commands=['start'], state="*")
 async def _(message: Message, state: FSMContext):
-    user_id = message.from_user.id
+    user_id = message.from_id
 
     # Запоминаем всех пользователей, что нажали /start
     if user_id not in all_users:
         all_users.add(user_id)
-        await send_to_all_users(f"Всего пользователей: {len(all_users)}")
+        await send_to_users(f"Всего пользователей: {len(all_users)}")
 
     await state.update_data({"user_id": user_id, "username": message.from_user.username})
     await message.answer("Привет! Напиши мне что-нибудь!")
     # то же самое что и
-    # await bot.send_message(message.from_user.id, "Привет! Напиши мне что-нибудь!")
+    # await bot.send_message(message.from_id, "Привет! Напиши мне что-нибудь!")
     await state.set_state("wait_for_message")
 
 
-async def send_to_all_users(text, parse_mode=None):
-    for user_id in tuple(all_users):
+async def send_to_users(text, users: Iterable[int] = None, parse_mode=None):
+    if users is None:
+        users = tuple(all_users)
+    else:
+        users = tuple(users)
+
+    for user_id in users:
         await bot.send_message(user_id, text, parse_mode=parse_mode)
 
 
 @dp.message_handler(state="wait_for_message")
 async def _(message: Message, state: FSMContext):
     # await message.answer("Спасибо за сообщение!")
-    # id_ = message.from_user.id # постоянный для аккаунта
+    # id_ = message.from_id # постоянный для аккаунта
     # message.from_user.username # пользователь может сменить его
     # text = f"@{message.from_user.username} написал(а):\n{message.text}"
 
     text = \
-        f"<a href='tg://user?id={message.from_user.id}'>" \
+        f"<a href='tg://user?id={message.from_id}'>" \
         f"пользователь</a> написал(а):\n{message.text}"
-
-    await send_to_all_users(text, parse_mode="HTML")
+    all_except_me = all_users - {message.from_id}
+    await send_to_users(text, users=all_except_me, parse_mode="HTML")
 
     # await message.answer(text, parse_mode="HTML")
 
